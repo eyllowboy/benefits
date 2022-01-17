75b068df1e609f5d6dec596599e440925d69157b
Andrei Rabchun <a.rabchun@andersenlab.com>
2022-01-17 11:00:16 +0300

    Created entities

---
+++ src/main/java/com/andersenlab/benefits/domain/RoleEntity.java
@@ -0,0 +1,37 @@
+package com.andersenlab.benefits.domain;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String code;
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		RoleEntity that = (RoleEntity) o;
+		return name.equals(that.name) && code.equals(that.code);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, code);
+	}
+}
---
+++ src/main/java/com/andersenlab/benefits/domain/UserEntity.java
@@ -0,0 +1,39 @@
+package com.andersenlab.benefits.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor()
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column
+	private String login;
+	
+	@ManyToOne
+	@JoinColumn(name = "role_id")
+	private RoleEntity roleEntity;
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		UserEntity that = (UserEntity) o;
+		return login.equals(that.login);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(login);
+	}
+}
