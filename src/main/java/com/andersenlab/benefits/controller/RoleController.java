7fbeafe2ea6007b33ce7fa68436c205af95a4a73
Andrei Rabchun <a.rabchun@andersenlab.com>
2022-01-25 11:12:37 +0300

    Fixed controller handler logic.

--- src/main/java/com/andersenlab/benefits/controller/RoleController.java
+++ src/main/java/com/andersenlab/benefits/controller/RoleController.java
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class RoleController {
-	private RoleService roleService;
+	private final RoleService roleService;
 	
 	@Autowired
 	public RoleController(RoleService roleService) {
@@ -22,51 +23,63 @@ public class RoleController {
 	
 	@GetMapping("/roles")
 	public List<RoleEntity> getRoles() {
-		return roleService.findAll();
+		return roleService.findAll()
+				.stream()
+				.map(x -> x.orElseThrow(
+						() -> new IllegalStateException("Having trouble getting data from the database")))
+				.toList();
 	}
 	
-	@GetMapping("/roles/{id}")
-	public RoleEntity getRole(@PathVariable @DecimalMin("1") Integer id) {
-		return roleService.findById(id);
+	@PutMapping("/roles")
+	public void updateRole(@RequestBody RoleEntity roleEntity) {
+		roleService.findById(roleEntity.getId())
+				.orElseThrow(() -> new IllegalStateException("Role with this id was not found in the database"));
+		
+		Optional<RoleEntity> roleEntityWithExistCode = roleService.findByCode(roleEntity.getCode());
+		
+		if (roleEntityWithExistCode.isEmpty()) {
+			roleService.updateRoleEntity(roleEntity.getId(), roleEntity.getName(), roleEntity.getCode());
+			
+		} else {
+			throw new IllegalStateException("Role with such 'code' is already exists");
+		}
 	}
 	
 	@PostMapping("/roles")
 	public ResponseEntity<?> addRole(
-			@RequestParam(value = "name", required = false)
+			@RequestParam(value = "name")
 			@Size(min = 3, max = 25, message = "Name length is between 3 and 25 characters.")
 					String name,
-			@RequestParam(value = "code", required = false)
+			@RequestParam(value = "code")
 			@Size(min = 3, max = 20, message = "Code length is between 3 and 20 characters.")
 					String code) {
 		
-		RoleEntity roleEntityWithExistCode = roleService.findByCode(code);
+		roleService.findByCode(code)
+				.ifPresent(roleEntity -> {throw new RuntimeException("Role with such 'code' is already exists");});
 		
-		if (roleEntityWithExistCode == null) {
-			roleEntityWithExistCode = new RoleEntity(name, code);
-			
-			return new ResponseEntity<>(roleService.save(roleEntityWithExistCode), HttpStatus.CREATED);
-			
-		} else {
-			throw new IllegalStateException("Role with such 'code' is already exists");
-		}
+		RoleEntity roleEntityToWrite = new RoleEntity(name, code);
+		Optional<RoleEntity> savedRoleEntity = roleService.save(roleEntityToWrite);
+		
+		return new ResponseEntity<>(
+				savedRoleEntity.orElseThrow(
+						() -> new IllegalStateException("Having trouble saving to the database"))
+				, HttpStatus.CREATED);
 	}
 	
-	@PutMapping("/roles")
-	public ResponseEntity<?> updateRole(@RequestBody RoleEntity roleEntity) {
-		roleService.findById(roleEntity.getId());
+	@GetMapping("/roles/{id}")
+	public RoleEntity getRole(@PathVariable @DecimalMin("1") Long id) {
+		Optional<RoleEntity> roleEntity = roleService.findById(id);
 		
-		RoleEntity roleEntityWithExistCode = roleService.findByCode(roleEntity.getCode());
-		
-		if (roleEntityWithExistCode == null) {
-			return new ResponseEntity<>(roleService.save(roleEntity), HttpStatus.OK);
-			
-		} else {
-			throw new IllegalStateException("Role with such 'code' is already exists");
-		}
+		return roleEntity.orElseThrow(
+				() -> new IllegalStateException("Role with this id was not found in the database"));
 	}
 	
 	@DeleteMapping("/roles/{id}")
-	public void deleteRole(@PathVariable @DecimalMin("1") Integer id) {
-		roleService.deleteRole(id);
+	public void deleteRole(@PathVariable @DecimalMin("1") Long id) {
+		roleService.findById(id)
+				.orElseThrow(
+						() -> new IllegalStateException("Role with this id was not found in the database"));
+		
+		roleService.delete(id);
 	}
 }
--- src/main/java/com/andersenlab/benefits/controller/UserController.java
+++ src/main/java/com/andersenlab/benefits/controller/UserController.java
@@ -12,63 +12,86 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
-	private RoleService roleService;
-	private UserService userService;
+	private final UserService userService;
+	private final RoleService roleService;
 	
 	@Autowired
-	public UserController(RoleService roleService, UserService userService) {
-		this.roleService = roleService;
+	public UserController(UserService userService, RoleService roleService) {
 		this.userService = userService;
+		this.roleService = roleService;
 	}
 	
 	@GetMapping("/users")
 	public List<UserEntity> getUsers() {
-		return userService.findAll();
+		return userService.findAll()
+				.stream()
+				.map(x -> x.orElseThrow(
+				() -> new IllegalStateException("Having trouble getting data from the database")))
+				.toList();
+	}
+	
+	@PutMapping("/users")
+	public void updateUser(@RequestBody UserEntity userEntity) {
+		Optional<UserEntity> userEntityInDataBaseForUpdate = userService.findById(userEntity.getId());
+		userEntityInDataBaseForUpdate
+				.orElseThrow(() -> new IllegalStateException("User with this id was not found in the database"));
+		
+		Optional<RoleEntity> roleEntityInDataBase = roleService.findById(userEntity.getRoleEntity().getId());
+		roleEntityInDataBase
+				.orElseThrow(() -> new IllegalStateException("Role with this id was not found in the database"));
+		
+		Optional<UserEntity> userEntityWithSameLogin = userService.findByLogin(userEntity.getLogin());
+		
+		if (userEntityWithSameLogin.isPresent()
+				&& !userEntityWithSameLogin.get().getId().equals(userEntity.getId())) {
+			throw new IllegalStateException("User with such 'login' is already exists");
+		} else {
+			userService.updateUserEntity(userEntity.getId(),
+					userEntity.getLogin(),
+					userEntity.getRoleEntity());
+		}
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<?> addUser(
+			@RequestParam(value = "login")
+			@Size(min = 3, max = 20, message = "Login length is between 3 and 20 characters.")
+					String login,
+			@RequestParam(value = "roleId") Long roleId) {
+		
+		userService.findByLogin(login)
+				.ifPresent(userEntity -> {throw new RuntimeException("User with such 'login' is already exists");});
+		
+		Optional<RoleEntity> roleEntityInDataBase = roleService.findById(roleId);
+		roleEntityInDataBase
+				.orElseThrow(() -> new IllegalStateException("Role with this id was not found in the database"));
+		
+		Optional<UserEntity> savedUserEntity = userService.save(new UserEntity(login, roleEntityInDataBase.get()));
+		
+		return new ResponseEntity<>(
+				savedUserEntity.orElseThrow(
+						() -> new IllegalStateException("Having trouble saving to the database"))
+				, HttpStatus.CREATED);
 	}
 	
 	@GetMapping("/users/{id}")
-	public UserEntity getUser(@PathVariable @DecimalMin("1") Integer id) {
-		return userService.findById(id);
-	}
-
-	@PostMapping("/users")
-	public ResponseEntity<?> addUser(
-			@RequestParam(value = "login", required = false)
-			@Size(min = 3, max = 20, message = "Login length is between 3 and 20 characters.")
-					String login,
-			@RequestBody RoleEntity roleEntity) {
-
-		UserEntity userEntityWithExistLogin = userService.findByLogin(login);
-
-		if (userEntityWithExistLogin == null) {
-			userEntityWithExistLogin = new UserEntity(login, roleEntity);
-
-			return new ResponseEntity<>(userService.save(userEntityWithExistLogin), HttpStatus.CREATED);
-
-		} else {
-			throw new IllegalStateException("User with such 'login' is already exists");
-		}
-	}
-
-	@PutMapping("/users")
-	public ResponseEntity<?> updateUser(@RequestBody UserEntity userEntity) {
-		userService.findById(userEntity.getId());
+	public UserEntity getUser(@PathVariable @DecimalMin("1") Long id) {
+		Optional<UserEntity> userEntity = userService.findById(id);
 		
-		UserEntity userEntityWithExistLogin = userService.findByLogin(userEntity.getLogin());
-		
-		if (userEntityWithExistLogin == null) {
-			return new ResponseEntity<>(userService.save(userEntity), HttpStatus.OK);
-
-		} else {
-			throw new IllegalStateException("User with such 'login' is already exists");
-		}
+		return userEntity.orElseThrow(
+				() -> new IllegalStateException("User with this id was not found in the database"));
 	}
-
+	
 	@DeleteMapping("/users/{id}")
-	public void deleteUser(@PathVariable @DecimalMin("1") Integer id) {
-		userService.deleteUser(id);
+	public void deleteUser(@PathVariable @DecimalMin("1") Long id) {
+		userService.findById(id)
+				.orElseThrow(
+						() -> new IllegalStateException("User with this id was not found in the database"));
+		
+		userService.delete(id);
 	}
 }
