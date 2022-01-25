8f581f5be39f9c3f8673b2aad4846378c17668f3
Andrei Rabchun <a.rabchun@andersenlab.com>
2022-01-25 10:39:35 +0300

    Create new methods updateUserEntity and updateRoleEntity

--- src/main/java/com/andersenlab/benefits/repository/RoleRepository.java
+++ src/main/java/com/andersenlab/benefits/repository/RoleRepository.java
@@ -2,9 +2,21 @@ package com.andersenlab.benefits.repository;
 
 import com.andersenlab.benefits.domain.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
-public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
 	RoleEntity findByCode(String code);
+	
+	@Modifying
+	@Transactional
+	@Query("update RoleEntity r set r.name = :name, r.code = :code where r.id = :id")
+	void updateRoleEntity(@Param(value = "id") Long id,
+	                      @Param(value = "name") String name,
+	                      @Param(value = "code") String code);
 }
--- src/main/java/com/andersenlab/benefits/repository/UserRepository.java
+++ src/main/java/com/andersenlab/benefits/repository/UserRepository.java
@@ -1,10 +1,23 @@
 package com.andersenlab.benefits.repository;
 
+import com.andersenlab.benefits.domain.RoleEntity;
 import com.andersenlab.benefits.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 	UserEntity findByLogin(String login);
+	
+	@Modifying
+	@Transactional
+	@Query("update UserEntity u set u.login = :login, u.roleEntity = :roleEntity where u.id = :id")
+	void updateUserEntity(@Param(value = "id") Long id,
+	                      @Param(value = "login") String login,
+	                      @Param(value = "roleEntity") RoleEntity roleEntity);
 }
