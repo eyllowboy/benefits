b1b95559dee3720a31c6afcc429323178da05ce3
Aleksei Sidorin <sidorinaleksei@gmail.com>
2022-02-09 10:56:39 +0300

    add descriptions in service

--- src/main/java/com/andersenlab/benefits/service/DiscountService.java
+++ src/main/java/com/andersenlab/benefits/service/DiscountService.java
@@ -17,31 +17,41 @@ import java.util.Optional;
 public interface DiscountService {
 
     /**
-     * @param id  the id of record in the database, not null
+     * Method allows us finds the discount by id.
+     *
+     * @param id the id of record in the database, not null
      * @return the discount corresponding given id from database, error if id not found
      */
     Optional<Discount> findByIdDiscount(final Long id);
 
     /**
+     * Method allows us finds all discount.
+     *
      * @return the list of discounts from database, error if not processed
      */
     List<Optional<Discount>> findAllDiscounts();
 
     /**
+     * Method allows creates new the discount.
+     *
      * @param discount contains information to create a new record in the database, not null
      * @return the entity corresponding new record in the database, error if consistency conditions are not met
      */
     Optional<Discount> createDiscount(final Discount discount);
 
     /**
-     * @param id  the id of record in the database, not null
+     * Method allows updates the discount by id.
+     *
+     * @param id       the id of record in the database, not null
      * @param discount contains information to the updated record in the database, not null
      * @return the entity corresponding updated record in the database, error if consistency conditions are not met
      */
     Optional<Discount> updateDiscountById(final Long id, final Discount discount);
 
     /**
-     * @param id  the id of record in the database, not null
+     * Method allows deleted the discount by id.
+     *
+     * @param id the id of record in the database, not null
      */
     void deleteDiscountById(final Long id);
 
