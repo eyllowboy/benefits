19a0385e7d06217b86bb65c0d24c298130a1864f
Andrei Rabchun <a.rabchun@andersenlab.com>
2022-02-17 11:02:47 +0300

    correct ABenefitsApplicationTests

--- src/main/java/com/andersenlab/benefits/security/WebSecurityConfig.java
+++ src/main/java/com/andersenlab/benefits/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticatio
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
+@Profile({ "!test" })
 @KeycloakConfiguration
 public class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     private static final String[] AUTH_WHITELIST = {
