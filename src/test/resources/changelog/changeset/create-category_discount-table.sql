5bb91135efb4b2b85c13a91baab6578adb7c0d26
dvppost <dvppost@gmail.com>
2022-02-24 22:43:41 +0300

    Category tests working

--- src/test/resources/changelog/changeset/create-category_discount-table.sql
+++ src/test/resources/changelog/changeset/create-category_discount-table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS category_discount (
-  cd_id BIGSERIAL PRIMARY KEY,
-  cd_category_id BIGINT,
-  cd_discount_id BIGINT,
-  FOREIGN KEY (cd_discount_id) REFERENCES discounts(id),
-  FOREIGN KEY (cd_category_id) REFERENCES categories(id));
+  id BIGSERIAL PRIMARY KEY,
+  category_id BIGINT,
+  discount_id BIGINT,
+  FOREIGN KEY (discount_id) REFERENCES discounts(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id));
 
-INSERT INTO category_discount (cd_category_id, cd_discount_id) VALUES
+INSERT INTO category_discount (category_id, discount_id) VALUES
     (1, 1),
     (1, 2),
     (2, 3),
